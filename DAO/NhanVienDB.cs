d94d4a16c67502b256f4af6ec38f6c3663c5b52a
thidinhxm <thidinhxm@gmail.com>
2022-07-06 22:12:01 +0700

    pcbs

---
+++ DAO/NhanVienDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class NhanVienDB
+    {
+        private static HTTiemChungDBContext context = new HTTiemChungDBContext();
+        public static List<NhanVien> LayDSNhanVien()
+        {
+            return context.NhanViens.ToList();
+        }
+        public static NhanVien LayChiTietNhanVien(String MaNV) 
+        {
+            return context.NhanViens.Find(MaNV);
+        }
+
+    }
+}
