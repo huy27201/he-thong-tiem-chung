ad521f26b5e24efc3460aaa11a340c15e285bb8b
thidinhxm <thidinhxm@gmail.com>
2022-07-07 00:06:02 +0700

    merge y

--- Models/HTTiemChungDBContext.cs
+++ Models/HTTiemChungDBContext.cs
@@ -38,11 +38,7 @@ namespace Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-<<<<<<< HEAD
-                optionsBuilder.UseSqlServer(@"Server=DESKTOP-1E3RHRQ\SQLEXPRESS;Database=HTTiemChungDB;Trusted_Connection=True;");
-=======
                 optionsBuilder.UseSqlServer(@"Server=HIT\SQLEXPRESS;Database=HTTiemChungDB;Trusted_Connection=True;User ID = HIT\Admin");
->>>>>>> master
             }
         }
 
